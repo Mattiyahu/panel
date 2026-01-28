082c08dfde8c0e6e4c9c94ee3da463db752c782d
Mattiyahu <thepancakeface823@gmail.com>
2026-01-28 11:34:11 -0300

    Create missajoiner.py

---
+++ missajoiner.py
@@ -0,0 +1,858 @@
+MSA
+.emiessiah
+My Lord.... i always combated side by side
+
+Dessilenciar
+Opções de entrada
+
+Desativar áudio
+Opções de saída
+
+Configurações de usuário
+Mensagem direta
+
+Gusta
+paralelo___
+Buscar paralelo___
+
+Chat do canal
+26 de janeiro de 2026
+
+Gusta
+
+BSBR
+ — 26/01/2026 13:29segunda-feira, 26 de janeiro de 2026 13:29
+claro
+[13:29]segunda-feira, 26 de janeiro de 2026 13:29
+so jogar
+
+MSA
+
+LARA
+ — 26/01/2026 13:30segunda-feira, 26 de janeiro de 2026 13:30
+KKKKKKKKKKKK
+
+Gusta
+
+BSBR
+ — 26/01/2026 13:30segunda-feira, 26 de janeiro de 2026 13:30
+gg
+[13:30]segunda-feira, 26 de janeiro de 2026 13:30
+kkkkkkk
+
+MSA
+
+LARA
+ — 26/01/2026 13:36segunda-feira, 26 de janeiro de 2026 13:36
+foi
+
+Gusta
+
+BSBR
+ — 26/01/2026 13:36segunda-feira, 26 de janeiro de 2026 13:36
+ja?
+
+MSA
+
+LARA
+ — 26/01/2026 13:36segunda-feira, 26 de janeiro de 2026 13:36
+ss
+
+Gusta
+
+BSBR
+ — 26/01/2026 13:36segunda-feira, 26 de janeiro de 2026 13:36
+krlh
+[13:36]segunda-feira, 26 de janeiro de 2026 13:36
+q velo é essa
+[13:36]segunda-feira, 26 de janeiro de 2026 13:36
+me arruma um beequipe ai
+
+MSA
+
+LARA
+ — 26/01/2026 13:37segunda-feira, 26 de janeiro de 2026 13:37
+qual?
+
+Gusta
+
+BSBR
+ — 26/01/2026 13:37segunda-feira, 26 de janeiro de 2026 13:37
+sendo blue
+
+MSA
+
+LARA
+ — 26/01/2026 13:37segunda-feira, 26 de janeiro de 2026 13:37
+xov na loja
+[13:37]segunda-feira, 26 de janeiro de 2026 13:37
+manda trade dnv
+
+Gusta
+
+BSBR
+ — 26/01/2026 13:38segunda-feira, 26 de janeiro de 2026 13:38
+ae ja po sair do sv
+[13:38]segunda-feira, 26 de janeiro de 2026 13:38
+vou te passar o cod aqui
+
+MSA
+
+LARA
+ — 26/01/2026 13:39segunda-feira, 26 de janeiro de 2026 13:39
+dps tu olha o código do vsphone lá?
+[13:39]segunda-feira, 26 de janeiro de 2026 13:39
+eu refiz
+#!/usr/bin/env python3
+import os
+import subprocess
+import time
+import requests
+import datetime
+Expandir
+Expandir (245 linhas)
+Visualize o arquivo inteiro
+message.txt
+message.txt (11 KB)
+11 KB
+message.txt (11 KB)
+Baixar message.txt (11 KB)Alterar idioma
+Excluir
+[13:39]segunda-feira, 26 de janeiro de 2026 13:39
+mas ficou bem porcaria
+[13:39]segunda-feira, 26 de janeiro de 2026 13:39
+e ta fechando todos os roblox em vez do q deu defeito
+A mensagem original foi excluída
+
+MSA
+
+LARA
+ — 26/01/2026 13:40segunda-feira, 26 de janeiro de 2026 13:40
+esse é qual?
+
+@MSA
+
+LARA
+esse é qual?
+
+Gusta
+
+BSBR
+ — 26/01/2026 13:40segunda-feira, 26 de janeiro de 2026 13:40
+fuzzy bee
+[13:40]segunda-feira, 26 de janeiro de 2026 13:40
+da tad jaja te mnd
+
+MSA
+
+LARA
+ — 26/01/2026 13:40segunda-feira, 26 de janeiro de 2026 13:40
+ta
+
+MSA
+
+LARA
+ — 26/01/2026 13:56segunda-feira, 26 de janeiro de 2026 13:56
+ta ai?
+
+Gusta
+
+BSBR
+ — 26/01/2026 14:05segunda-feira, 26 de janeiro de 2026 14:05
+ss
+
+MSA
+
+LARA
+ — 26/01/2026 14:12segunda-feira, 26 de janeiro de 2026 14:12
+Um amigo me passou a mesma config
+[14:12]segunda-feira, 26 de janeiro de 2026 14:12
+Q vc
+[14:12]segunda-feira, 26 de janeiro de 2026 14:12
+ME mandou
+
+Gusta
+
+BSBR
+ — 26/01/2026 14:14segunda-feira, 26 de janeiro de 2026 14:14
+ctz?
+[14:14]segunda-feira, 26 de janeiro de 2026 14:14
+mnd print ae
+[14:14]segunda-feira, 26 de janeiro de 2026 14:14
+oxi
+[14:14]segunda-feira, 26 de janeiro de 2026 14:14
+calma ai xo checar se é essa config msm
+[14:15]segunda-feira, 26 de janeiro de 2026 14:15
+[{"Data":["38217","BeesBuzz123","BopMaster","Connoisseur","Crawlers","Nectar","Roof","Wax"],"Type":"Codes"},{"Group":true,"Type":"Codes","Data":"777"},{"Group":true,"Type":"Codes","Data":"ClubBean","Bees":15},{"Data":["Black Bear","Science Bear","Spirit Bear"],"Type":"Present"},{"Data":["Dandelion Field","Bamboo Field","Pineapple Patch","Pine Tree Forest"],"Type":"Field"},{"Data":["Tabby","Cobalt","Festive","Photon"],"Type":"Event Bee"},{"Data":50,"Type":"Bees"},{"Type":"Purchase","Bees":0,"BlenderTickets":false,"Data":["Backpack","Magnet","Canister","Vacuum","Belt Pocket","Basic Boots","Compressor","Super-Scooper","Helmet","Port-O-Hive","Electro-Magnet","Honey Dipper","Propeller Hat","Hiking Boots","Belt Bag","Bubble Wand","Basic Sprinkler","PlasticPlanter","Beekeeper's Mask","Beekeeper's Boots","Mondo Belt Bag","Silver Soakers","Elite Blue Guard","Elite Red Guard","Glider","Porcelain Port-O-Hive","Porcelain Dipper","Blue ClayPlanter","Red ClayPlanter","Bubble Mask","Cobalt Guard","Golden Gushers","TackyPlanter","Diamond Mask","Petal Belt","Crimson Guard","Diamond Drenchers","Coconut Canister",","Petal Wand","The Supreme Saturator","Gummy Mask",","Tide Popper","PetalPlanter","Heat-TreatedPlanter","Coconut Belt","PlentyPlanter"],"Craft":true}]
+[14:15]segunda-feira, 26 de janeiro de 2026 14:15
+perdao
+[14:15]segunda-feira, 26 de janeiro de 2026 14:15
+eu mandei a mais demorada
+[14:15]segunda-feira, 26 de janeiro de 2026 14:15
+essa é a rush se n me engano
+
+MSA
+
+LARA
+ — 26/01/2026 14:17segunda-feira, 26 de janeiro de 2026 14:17
+Bro
+
+Gusta
+
+BSBR
+ — 26/01/2026 14:17segunda-feira, 26 de janeiro de 2026 14:17
+hm
+[14:18]segunda-feira, 26 de janeiro de 2026 14:18
+mn e olha direito a diferença do meu por do teu amigo viu
+[14:19]segunda-feira, 26 de janeiro de 2026 14:19
+esses bglh de auto progression sao mt iguais, e mudam algumas coisas do rush pro normal
+[14:21]segunda-feira, 26 de janeiro de 2026 14:21
+vou ter q sair agr qlq coisa em marca
+[14:21]segunda-feira, 26 de janeiro de 2026 14:21
+e qnd eu chegar vou te ensinar codar esses bglh
+28 de janeiro de 2026
+
+Gusta
+
+BSBR
+ — 00:30quarta-feira, 28 de janeiro de 2026 00:30
+@MSA
+[00:30]quarta-feira, 28 de janeiro de 2026 00:30
+Ta on?
+
+MSA
+
+LARA
+ — 00:34quarta-feira, 28 de janeiro de 2026 00:34
+Tava indo dormir
+
+Gusta
+
+BSBR
+ — 00:42quarta-feira, 28 de janeiro de 2026 00:42
+Amanhã vou te ensinar os bglh
+[00:42]quarta-feira, 28 de janeiro de 2026 00:42
+Tinha esquecido de tu
+[00:42]quarta-feira, 28 de janeiro de 2026 00:42
+Kkkk
+
+MSA
+
+LARA
+ — 10:51quarta-feira, 28 de janeiro de 2026 10:51
+hm
+[10:51]quarta-feira, 28 de janeiro de 2026 10:51
+se tem 10 conta com 40 bee?
+
+@MSA
+
+LARA
+se tem 10 conta com 40 bee?
+
+Gusta
+
+BSBR
+ — 10:51quarta-feira, 28 de janeiro de 2026 10:51
+sim-
+
+MSA
+
+LARA
+ — 10:51quarta-feira, 28 de janeiro de 2026 10:51
+tem oq de item nelas
+
+@MSA
+
+LARA
+tem oq de item nelas
+
+Gusta
+
+BSBR
+ — 10:52quarta-feira, 28 de janeiro de 2026 10:52
+lvl 10
+[10:52]quarta-feira, 28 de janeiro de 2026 10:52
+5 mitica aleatoria
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+[10:52]quarta-feira, 28 de janeiro de 2026 10:52
+tudo da area 25
+
+MSA
+
+LARA
+ — 10:52quarta-feira, 28 de janeiro de 2026 10:52
+tem oq de mask
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Editar
+Encaminhar
+Mais
+
+@MSA
+
+LARA
+tem oq de mask
+
+Gusta
+
+BSBR
+ — 10:52quarta-feira, 28 de janeiro de 2026 10:52
+bubble mask
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+
+MSA
+
+LARA
+ — 10:52quarta-feira, 28 de janeiro de 2026 10:52
+eu tenho 6 bubble alt
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Editar
+Encaminhar
+Mais
+[10:52]quarta-feira, 28 de janeiro de 2026 10:52
+mn
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Editar
+Encaminhar
+Mais
+[10:52]quarta-feira, 28 de janeiro de 2026 10:52
+se me passou o import errado dnv
+[10:52]quarta-feira, 28 de janeiro de 2026 10:52
+
+[10:52]quarta-feira, 28 de janeiro de 2026 10:52
+n quero ser tapiado
+
+Gusta
+
+BSBR
+ — 10:52quarta-feira, 28 de janeiro de 2026 10:52
+ox
+[10:53]quarta-feira, 28 de janeiro de 2026 10:53
+o import pra min  ta indo normal cz (editado)quarta-feira, 28 de janeiro de 2026 10:53
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+[10:53]quarta-feira, 28 de janeiro de 2026 10:53
+quer que eu crie um pra ti?
+
+MSA
+
+LARA
+ — 10:55quarta-feira, 28 de janeiro de 2026 10:55
+oq
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Editar
+Encaminhar
+Mais
+
+Gusta
+
+BSBR
+ — 10:55quarta-feira, 28 de janeiro de 2026 10:55
+import
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+[10:55]quarta-feira, 28 de janeiro de 2026 10:55
+um q so tu tenha
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+
+MSA
+
+LARA
+ — 10:56quarta-feira, 28 de janeiro de 2026 10:56
+sla mn
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Editar
+Encaminhar
+Mais
+[10:57]quarta-feira, 28 de janeiro de 2026 10:57
+to desentendido
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Editar
+Encaminhar
+Mais
+
+MSA
+
+LARA
+ — 11:24quarta-feira, 28 de janeiro de 2026 11:24
+tu tbm n olhou meu codigo
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Editar
+Encaminhar
+Mais
+
+Gusta
+
+BSBR
+ — 11:24quarta-feira, 28 de janeiro de 2026 11:24
+vdd né
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+[11:25]quarta-feira, 28 de janeiro de 2026 11:25
+mn
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+[11:25]quarta-feira, 28 de janeiro de 2026 11:25
+tu ta forçando relaunch
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+[11:27]quarta-feira, 28 de janeiro de 2026 11:27
+#!/usr/bin/env python3
+import os, subprocess, time, requests, datetime, json, threading
+from rich.console import Console
+from rich.panel import Panel
+from rich.table import Table
+from rich.prompt import Prompt
+Expandir
+Expandir (224 linhas)
+Visualize o arquivo inteiro
+message.txt
+message.txt (8 KB)
+8 KB
+message.txt (8 KB)
+Baixar message.txt (8 KB)Alterar idioma
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+[11:28]quarta-feira, 28 de janeiro de 2026 11:28
+@MSA
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+[11:28]quarta-feira, 28 de janeiro de 2026 11:28
+ve se da certo ae
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+
+MSA
+
+LARA
+ — 11:32quarta-feira, 28 de janeiro de 2026 11:32
+perae
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Editar
+Encaminhar
+Mais
+[11:32]quarta-feira, 28 de janeiro de 2026 11:32
+oq tava errado?
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Editar
+Encaminhar
+Mais
+[11:32]quarta-feira, 28 de janeiro de 2026 11:32
+alem do relaunch
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Editar
+Encaminhar
+Mais
+NOVO
+
+Gusta
+
+BSBR
+ — 11:33quarta-feira, 28 de janeiro de 2026 11:33
+erro na execuçao
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+[11:33]quarta-feira, 28 de janeiro de 2026 11:33
+a ui tava errada
+:sob:
+Clique para reagir
+:people_hugging:
+Clique para reagir
+:weewoo~1:
+Clique para reagir
+Adicionar reação
+Responder
+Encaminhar
+Mais
+[11:33]quarta-feira, 28 de janeiro de 2026 11:33
+eo force stop
+
+Conversar em @Gusta
+﻿
+Adicionar emoji, GIFs, Figurinhas
+Adicionar mídia
+CTRL
+SHIFT
+U
+Reforce os parças! Dê vantagens superbacanas no chat com o Nitro.
+#!/usr/bin/env python3
+import os, subprocess, time, requests, datetime, json, threading
+from rich.console import Console
+from rich.panel import Panel
+from rich.table import Table
+from rich.prompt import Prompt
+from rich.layout import Layout
+from rich.live import Live
+from rich.text import Text
+from rich.align import Align
+from rich import print as rprint
+
+console = Console()
+CONFIG_FILE = "config.json"
+
+CHECK_INTERVAL = 3
+COOLDOWN_TIME = 300          # 5 minutos
+CPU_IDLE_LIMIT = 1.0        # CPU mínima aceitável
+MAX_IDLE_CYCLES = 30        # 30 x 3s = 90s reais travado
+
+# ================= CONFIG =================
+def load_config():
+    if os.path.exists(CONFIG_FILE):
+        try:
+            with open(CONFIG_FILE, "r") as f:
+                return json.load(f)
+        except:
+            pass
+    return {"vip_link": "", "webhook_url": ""}
+
+def save_config(cfg):
+    with open(CONFIG_FILE, "w") as f:
+        json.dump(cfg, f, indent=4)
+
+cfg = load_config()
+VIP_LINK = cfg.get("vip_link", "")
+WEBHOOK_URL = cfg.get("webhook_url", "")
+
+# ================= ROBLOX INSTANCE =================
+class RobloxInstance:
+    def __init__(self, package, manager):
+        self.package = package
+        self.manager = manager
+        self.pid = ""
+        self.cpu = 0.0
+        self.status = "INIT"
+        self.last_action = "-"
+        self.cooldown_until = 0
+        self.idle_counter = 0
+        self.running = True
+
+    def adb(self, cmd):
+        try:
+            return subprocess.check_output(
+                f"adb shell {cmd}",
+                shell=True,
+                stderr=subprocess.DEVNULL,
+                timeout=4
+            ).decode().strip()
+        except:
+            return ""
+
+    def update_status(self):
+        self.pid = self.adb(f"pidof {self.package}")
+
+        if not self.pid:
+            self.status = "BACKGROUND"
+            self.cpu = 0.0
+            return
+
+        top = self.adb(f"top -n 1 -p {self.pid}")
+        cpu = 0.0
+        for line in top.splitlines():
+            if self.pid in line:
+                for part in line.split():
+                    if "%" in part:
+                        cpu = float(part.replace("%", "").replace(",", "."))
+                        break
+        self.cpu = cpu
+
+        if time.time() < self.cooldown_until:
+            self.status = "STABILIZING"
+            return
+
+        if self.cpu > CPU_IDLE_LIMIT:
+            self.status = "RUNNING"
+            self.idle_counter = 0
+        else:
+            self.status = "IDLE"
+            self.idle_counter += 1
+
+    def relaunch(self, reason):
+        self.last_action = reason
+        self.manager.log(f"{self.package} → RELAUNCH ({reason})")
+
+        if WEBHOOK_URL:
+            try:
+                requests.post(WEBHOOK_URL, json={"content": f"🔄 **ROBLOX** `{self.package}` → {reason}"}, timeout=3)
+            except:
+                pass
+
+        # ❌ SEM FORCE-STOP
+        self.adb(f"am start -a android.intent.action.VIEW -d '{VIP_LINK}' {self.package}")
+        self.cooldown_until = time.time() + COOLDOWN_TIME
+        self.idle_counter = 0
+
+    def loop(self):
+        while self.running and self.manager.running:
+            self.update_status()
+
+            if self.status == "IDLE" and self.idle_counter >= MAX_IDLE_CYCLES:
+                self.relaunch("Travado > 90s")
+
+            time.sleep(CHECK_INTERVAL)
+
+# ================= MANAGER =================
+class RE_PHONE:
+    def __init__(self):
+        self.instances = {}
+        self.running = False
+        self.logs = []
+
+    def log(self, msg):
+        t = datetime.datetime.now().strftime("%H:%M:%S")
+        self.logs.append(f"[{t}] {msg}")
+        if len(self.logs) > 8:
+            self.logs.pop(0)
+
+    def force_portrait(self):
+        subprocess.run("adb shell settings put system accelerometer_rotation 0", shell=True)
+        subprocess.run("adb shell settings put system user_rotation 0", shell=True)
+
+    def start(self):
+        if not VIP_LINK:
+            rprint("[red]Configure o VIP LINK primeiro[/red]")
+            time.sleep(2)
+            return
+
+        self.force_portrait()
+        self.running = True
+
+        try:
+            out = subprocess.check_output("adb shell pm list packages | grep roblox", shell=True).decode()
+            pkgs = [l.replace("package:", "").strip() for l in out.splitlines()]
+        except:
+            pkgs = []
+
+        if not pkgs:
+            rprint("[red]Nenhum Roblox encontrado[/red]")
+            time.sleep(2)
+            return
+
+        for p in pkgs:
+            inst = RobloxInstance(p, self)
+            self.instances[p] = inst
+            threading.Thread(target=inst.loop, daemon=True).start()
+
+        with Live(self.render(), refresh_per_second=4, screen=True) as live:
+            try:
+                while self.running:
+                    live.update(self.render())
+                    time.sleep(0.25)
+            except KeyboardInterrupt:
+                self.running = False
+
+    def render(self):
+        layout = Layout()
+        layout.split_column(
+            Layout(name="header", size=3),
+            Layout(name="body", size=12),
+            Layout(name="footer", size=8)
+        )
+
+        layout["header"].update(
+            Panel(Align.center(Text("RE_PHONE v7 SAFE – VS PHONE", style="bold red")), border_style="red")
+        )
+
+        table = Table(expand=True, border_style="red", header_style="bold red")
+        table.add_column("PACOTE")
+        table.add_column("CPU", justify="center")
+        table.add_column("STATUS", justify="center")
+        table.add_column("AÇÃO", justify="center")
+
+        for inst in self.instances.values():
+            color = "green" if inst.status == "RUNNING" else "yellow" if inst.status == "IDLE" else "cyan"
+            table.add_row(
+                inst.package.split(".")[-1],
+                f"{inst.cpu:.1f}%",
+                f"[{color}]{inst.status}[/{color}]",
+                inst.last_action
+            )
+
+        layout["body"].update(Panel(table, title="MONITOR", border_style="red"))
+        layout["footer"].update(Panel("\n".join(self.logs), title="LOGS", border_style="red"))
+        return layout
+
+# ================= MENU =================
+manager = RE_PHONE()
+
+def main():
+    global VIP_LINK, WEBHOOK_URL
+
+    while True:
+        console.clear()
+        rprint(Align.center("[bold red]RE_PHONE SAFE – VS PHONE[/bold red]\n"))
+
+        rprint(Panel(f"VIP: {VIP_LINK[:40]}...\nWebhook: {'OK' if WEBHOOK_URL else 'OFF'}", border_style="red"))
+
+        opt = Prompt.ask("1 Iniciar | 2 Config | 0 Sair", choices=["1", "2", "0"])
+
+        if opt == "1":
+            manager.start()
+
+        elif opt == "2":
+            VIP_LINK = Prompt.ask("VIP LINK", default=VIP_LINK)
+            WEBHOOK_URL = Prompt.ask("Webhook", default=WEBHOOK_URL)
+            save_config({"vip_link": VIP_LINK, "webhook_url": WEBHOOK_URL})
+
+        elif opt == "0":
+            break
+
+if __name__ == "__main__":
+    main()
